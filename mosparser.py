36c41de444d0791f0148a8eb2df09f96ed1356ac
nikmort <nikmort@ya.ru>
2017-04-20 01:35:09 +0300

    some optimization

--- mosparser.py
+++ mosparser.py
@@ -1,58 +1,63 @@
 from re import match, I
 from requests import get as requests_get
 from bs4 import BeautifulSoup
-from utils import *
 
 main_cite = 'http://www.mosopen.ru/streets'
-alphabet = ('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й',
-            'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
-            'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я')
 digits = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
 
-def get_a_tags(link):
+# Извлечь из страницы a-тэги, содержащие определенные подстроки в ссылке.
+# Возвращается генератор по этим тэгам.
+def get_tags(link, keyword):
     res = requests_get(link)
     soup = BeautifulSoup(res.text, 'html.parser')
-    return soup.find_all('a')
+    for tag in soup.find_all('a'):
+        if keyword in tag.get('href'):
+            yield tag
 
 
+# Получить словарь, где ключами будут районы Москвы,
+# а значениями - словари с ключами-улицами (см. ниже).
 def get_regions():
-    a_tags = get_a_tags(main_cite)
     regions = {}
-    for t in a_tags:
-        region_link = t.get('href')
-        if 'region/' in region_link:
-            region_name = t.get_text()
-            print('***{}***'.format(region_name))
-            if region_name not in regions:
-                regions[region_name] = get_streets(region_link)
+    for tag in get_tags(main_cite, 'region/'):
+        region_name = tag.get_text()
+        print('***{}***'.format(region_name))
+        if region_name not in regions:
+            regions[region_name] = get_streets(tag.get('href'))
     return regions
 
 
+# Получить словарь, где ключами улицы района,
+# а значениями - списки домов (см. ниже).
+# На вход подается ссылка на район.
 def get_streets(region_link):
-    a_tags = get_a_tags(region_link)
     streets = {}
-    for t in a_tags:
-        street_link = t.get('href')
-        if 'street/' in street_link:
-            street_parser = t.get_text().replace(',', '').split()
-            street_type = street_parser[-1]
-            street_name = ' '.join(street_parser[:-1])
-            street_tuple = (street_name, street_type)
-            streets[street_tuple] = get_houses(street_link)
+    for tag in get_tags(region_link, 'street/'):
+        street_parser = tag.get_text().replace(',', '').split()
+        street_type = street_parser[-1]
+        street_name = ' '.join(street_parser[:-1])
+        street_tuple = (street_name, street_type)
+        streets[street_tuple] = get_houses(tag.get('href'))
     return streets
 
 
+# Получить список, элементами которого будет словари,
+# характеризующий дом (см. ниже).
+# На вход подается ссылка на улицу.
 def get_houses(street_link):
-    a_tags = get_a_tags(street_link)
     houses = []
-    for t in a_tags:
-        house_link = t.get('href')
-        if 'address' in house_link:
-            houses.append(house_parser(t.get_text()))
+    for tag in get_tags(street_link, 'address.'):
+        houses.append(house_parser(tag.get_text()))
     return houses
 
 
+# Получить словарь со следующими ключами:
+# ind - номер дома,
+# / - дробь,
+# с - строение,
+# к - корпус
+# ft - полная адресная строка дома с сайта mosopen.
 def house_parser(house_text):
     full_text = house_text.replace('\xa0', ' ')
     parser = full_text.split(' ')
@@ -67,6 +72,8 @@ def house_parser(house_text):
     house['ft'] = full_text
     return house
 
+
+# Вспомогательная функция для парсинга номера дома.
 def index_parser(index_text):
     index = {}
     parser = index_text.split('/')
--- utils.py
+++ utils.py
@@ -1,17 +1,20 @@
 import pickle
 
+# Выводит список районов, их улиц и домов.
 def print_regions(regions):
     for region in regions:
         print('{}\n***{}***'.format(20 * '=', region))
         print_streets(regions[region])
 
 
+# Выводит список улиц и их домов.
 def print_streets(streets):
     for street in streets:
         print(', '.join(street))
         print_houses(streets[street])
 
 
+# Выводит список домов.
 def print_houses(houses):
     to_print = []
     for house in houses:
@@ -19,11 +22,13 @@ def print_houses(houses):
     print(', '.join(to_print))
 
 
+# Сохранить любой объект в файл.
 def save_obj(obj, filename):
     with open(filename + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 
+# Загрузить объект из файла.
 def load_obj(filename):
     with open(filename + '.pkl', 'rb') as f:
         return pickle.load(f)
