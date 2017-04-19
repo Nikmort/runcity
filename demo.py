30b5fa183fa2a55cbca28dc2884f0b0d13b2b2df
nikmort <nikmort@ya.ru>
2017-04-20 01:38:57 +0300

    comment edit

--- demo.py
+++ demo.py
@@ -1,13 +1,12 @@
 from utils import *
 from mosparser import *
 
-# print("Reading...")
 # regions = get_regions()
-# print("Reading end. Saving...")
 # save_obj(regions, 'moscow')
-# print("Saving end. Loading...")
 regions = load_obj('moscow')
-print("Loading end.")
+
+
+# Вывести данные по двум районам
 region_names = list(regions.keys())[:2]
 regions = {region_name: regions[region_name] for region_name in region_names}
 print_regions(regions)
